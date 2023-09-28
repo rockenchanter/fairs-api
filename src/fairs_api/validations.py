239d7f0821f815a5053fed01361e783f4f2c7570
Łukasz Flisak <lu.dev00@gmail.com>
2023-09-28 18:53:53 +0200

    Change how validation functions work

--- src/fairs_api/validations.py
+++ src/fairs_api/validations.py
@@ -1,43 +1,78 @@
-from flask import session
-
 import json
-import re
+import functools
+import datetime
+import regex as re  # better unicode support
+from os import path
 
-email_regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+# SWITCH TO REGEX LIBRARY
+email_regex = re.compile(
+    r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+digit_regex = re.compile(r"\d")
+upc_letter = re.compile(r"[\p{Lu}\p{Mark}]")
 
-with open("locale.json", "r") as f:
+file_path = path.join(path.dirname(path.abspath(__file__)), "locale.json")
+with open(file_path, "r") as f:
     data = json.load(f)
 
 
-def get_from_locale(locale: str, key: str):
+def required(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if args[0] is None or (isinstance(args[0], str) and len(args[0]) == 0):
+            return ("required",)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def get_from_locale(key: str, locale: str):
     return data[locale][key]
 
 
+@required
+def contains(text: str, regex: re.Pattern, locale_key: str):
+    if not re.search(regex, text):
+        return [locale_key,]
+
+
+@required
 def min_length(text: str, target: int):
     if len(text) < target:
-        return get_from_locale(session["locale"], "too_short")
+        return ["too_short", target]
     return None
 
 
+@required
 def max_length(text: str, target: int):
     if len(text) > target:
-        return get_from_locale(session["locale"], "too_long")
+        return ["too_long", target]
     return None
 
 
+@required
 def min(value: float, target: float):
     if value < target:
-        return get_from_locale(session["locale"], "too_small")
+        return ["too_small", target]
     return None
 
 
+@required
 def max(value: float, target: float):
     if value > target:
-        return get_from_locale(session["locale"], "too_big")
+        return ["too_big", target]
     return None
 
 
+@required
 def email(value):
     if not re.fullmatch(email_regex, value):
-        return get_from_locale(session["locale"], "invalid_email")
+        return ["invalid_email"]
+    return None
+
+
+@required
+def days_from_now(date: datetime.date, target: int):
+    min_date = datetime.timedelta(days=target) + datetime.date.today()
+    print(date < min_date)
+    if date < min_date:
+        return ["min_date", target]
     return None
